8ebef6b26987f0c56298525c6c28c626fc505b9a
galih rivanto <galih.rivanto@gmail.com>
2024-02-11 21:03:14 +0700

    initial. shadcn + bits-ui + tailwindcss

---
+++ src/lib/button.ts
@@ -0,0 +1,3 @@
+import * as button from "./components/ui/button"
+
+export button
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+</script>
+
+<div class="flex w-1/2 mx-auto mt-36">
+    <Button>Click me</Button>
+</div>
