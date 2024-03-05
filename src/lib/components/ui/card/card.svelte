a601bb989aaeb5d8f61cf988df5ffbef966af08d
galih rivanto <galih.rivanto@gmail.com>
2024-03-05 10:20:02 +0700

    chores: update card component

--- src/lib/components/ui/card/card.svelte
+++ src/lib/components/ui/card/card.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
 	import type { HTMLAttributes } from "svelte/elements";
-	import { cn } from "$lib/utils";
+	import { classIf, cn } from "$lib/utils";
 
-	type $$Props = HTMLAttributes<HTMLDivElement>;
+	type $$Props = HTMLAttributes<HTMLDivElement> & {
+		glass?: boolean;
+		shadow?: boolean;
+	}
 
 	let className: $$Props["class"] = undefined;
 	export { className as class };
+	export let glass: $$Props["glass"] = false
+	export let shadow: $$Props["shadow"] = false 
 </script>
 
 <div
-	class={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)}
+	class={cn(
+		"card bg-base-100", 
+		classIf(glass == true, "bg-base-100/20 border backdrop-blur border-base-100/40"),
+		classIf(shadow == true, "shadow-lg"),
+		className)}
 	{...$$restProps}
 >
 	<slot />
---
+++ src/routes/card/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import Card from "$lib/card";
+    import Button from "$lib/button";
+</script>
+
+<div class="flex flex-col w-1/2 mx-auto mt-36 gap-2 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-16">
+    <Card.Root class="w-64" glass>
+        <Card.Header>
+            <Card.Title>Test Title</Card.Title>
+            <Card.Description>Test Description</Card.Description>
+        </Card.Header>
+        <Card.Content>something content</Card.Content>
+        <Card.Footer class="gap-2">
+            <Button>Close</Button>
+            <Button color="primary">Confirm</Button>
+        </Card.Footer>
+    </Card.Root>
+
+    <Card.Root class="w-64">
+        <Card.Content>
+            something content
+            wowo woekwaeads
+        </Card.Content>
+    </Card.Root>
+
+    <Card.Root class="w-64" glass shadow>
+        <Card.Content>
+            something content
+            wowo woekwaeads
+        </Card.Content>
+    </Card.Root>
+</div>
